0ce9acb295003162fe9a7e46ad79e31c9e8c445c
ConDish <samioff19@gmail.com>
2023-04-18 12:54:36 -0500

    feat: project initial

--- App.tsx
+++ App.tsx
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import auth from '@react-native-firebase/auth'
+import { Box, NativeBaseProvider } from 'native-base'
 
 export default function App() {
+  const user = auth().currentUser
+  console.log(user)
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NativeBaseProvider>
+      <Box bg="red.300">Hello world</Box>
+    </NativeBaseProvider>
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
